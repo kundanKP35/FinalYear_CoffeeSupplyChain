c1f30a0bc1171d6d66ccd9cfacce6b08a4861362
TheDemantor <emailtovaibhavgupta@gmail.com>
2024-05-01 23:36:41 +0530

    Created pages for all four roles

--- client/src/pages/Farmer.jsx
+++ client/src/pages/Farmer.jsx
@@ -1,63 +1,47 @@
-import React, { useState } from 'react';
-import {useStateContext} from '../context';
-import HarvestItem from '../components/Farmer/HarvestItem';
-import ProcessItem from '../components/Farmer/ProcessItem';
-import PackItem from '../components/Farmer/PackItem';
-import SellItem from '../components/Farmer/SellItem';
+import React, { useState } from "react";
+import { useStateContext } from "../context";
+import FarmerList from "../components/FarmerTable";
+import Footer from "../components/Footer";
 
 const Farmer = () => {
-    const [newFarmerId, setNewFarmerId] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-    const [addFarmerResult, setAddFarmerResult] = useState(null);
-    const [error, setError] = useState(null);
+  const [newFarmerId, setNewFarmerId] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [addFarmerResult, setAddFarmerResult] = useState(null);
+  const [error, setError] = useState(null);
 
-    const { addNewFarmer } = useStateContext();
+  const { addNewFarmer } = useStateContext();
 
-    const handleFormFieldChange = (e) => {
-        setNewFarmerId(e.target.value);
-    };
+  const handleFormFieldChange = (e) => {
+    setNewFarmerId(e.target.value);
+  };
 
-    const handleAddFarmer = async (e) => {
-        e.preventDefault();
-        setIsLoading(true);
-        setError(null);
+  const handleAddFarmer = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError(null);
 
-        try {
-            const result = await addNewFarmer(newFarmerId);
-            setAddFarmerResult('Farmer added successfully');
-        } catch (error) {
-            console.error('Error adding farmer:', error);
-            setError('Error adding farmer');
-        } finally {
-            setIsLoading(false);
-        }
-    };
+    try {
+      const result = await addNewFarmer(newFarmerId);
+      setAddFarmerResult("Farmer added successfully");
+    } catch (error) {
+      console.error("Error adding farmer:", error);
+      setError("Error adding farmer");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    return (
-        <div>
-            <div>
-                <h3>Add Farmer</h3>
-                <form onSubmit={handleAddFarmer} className="w-full mt-[65px] flex flex-col gap-[30px]">
-                    <label htmlFor="newFarmerIdInput">Enter New Farmer ID:</label>
-                    <input type="text" id="newFarmerIdInput" name="newFarmerIdInput" required value={newFarmerId} onChange={handleFormFieldChange} />
-                    <button type="submit">Add Farmer</button>
-                </form>
-                {isLoading && <p>Loading...</p>}
-                {addFarmerResult && <p>{addFarmerResult}</p>}
-                {error && <p>Error: {error}</p>}
-            </div>
-
-            <div>
-                <h3>Farmer roles..</h3>
-                <div>
-                    <HarvestItem />
-                    <ProcessItem />
-                    <PackItem />
-                    <SellItem />
-                </div>
-            </div>
+  return (
+    <div className="text-white ">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="text-xl font-semibold dark:text-white my-8">
+          -&gt; Farmer Actions
         </div>
-    )
-}
+        <FarmerList />
+      </div>
+      <Footer role={"farmer"} />
+    </div>
+  );
+};
 
-export default Farmer
+export default Farmer;
